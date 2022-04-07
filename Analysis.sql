f5e2c359f9cd4861f1efa4b0cc25525e5494b2ae
bobthebeef <55220108+bobthebeef@users.noreply.github.com>
2022-04-07 17:58:56 -0400

    Question 3

--- Analysis.sql
+++ Analysis.sql
@@ -88,26 +88,28 @@ VALUES
 
 --Create Basic BMI dimension
 DROP TABLE IF EXISTS dbo.BMI
-CREATE TABLE dbo.BMI (MinBMI FLOAT,MaxBMI FLOAT,BMIGroup VARCHAR(30))
+CREATE TABLE dbo.BMI (MinBMI DECIMAL(18,4),MaxBMI DECIMAL(18,4),BMIGroup VARCHAR(30))
 INSERT INTO dbo.BMI (MinBMI,MaxBMI,BMIGroup)
 VALUES
-(0.0,17.9,'UNDERWEIGHT'),
-(18.0,25.9,'NORMAL'),
-(26.0,29.9,'OVERWEIGHT'),
+(0.0,18.0,'UNDERWEIGHT'),
+(18.0001,25.0,'NORMAL'),
+(25.0001,29.9999,'OVERWEIGHT'),
 (30.0,39.9,'OBESE'),
-(40.0,1000.,'MORBIDLY OBESE')
+(40.0,1000.0,'MORBIDLY OBESE')
 
 
 select * FROM dbo.bmi
 
 
-SELECT a.*,c.AgeGroup,CAST(weight/((height/100.0)*(height/100.0)) AS INT) BMI,d.BMIGroup FROM dbo.RepeatImaging a
+SELECT a.*,c.AgeGroup,CAST(weight/((height/100.0)*(height/100.0)) AS INT) BMI,d.BMIGroup 
+FROM dbo.RepeatImaging a
 INNER JOIN dbo.PatientData_Cleansed b
 on a.PatientID=b.PatientID
 INNER JOIN dbo.Age c
 ON b.age BETWEEN c.MinAge and c.MaxAge
 LEFT OUTER JOIN dbo.BMI d
-ON CAST(CAST(weight AS FLOAT)/((height/100.0)*(height/100.0)) AS FLOAT) BETWEEN CAST(d.MinBMI AS FLOAT) and CAST(d.MaxBMI AS FLOAT)
+ON CAST(CAST(weight AS FLOAT)/((height/100.0)*(height/100.0)) AS FLOAT) BETWEEN CAST(d.MinBMI AS DECIMAL(18,4)) and CAST(d.MaxBMI AS DECIMAL(18,4))
+
 
 DROP TABLE IF EXISTS dbo.TotalPatientsByAge
 SELECT AgeGroup,COUNT(1) Patients
@@ -150,7 +152,59 @@ INTO dbo.RepeatImagingByState
 FROM dbo.RepeatImaging a
 GROUP BY State
 
-SELECT a.State,a.RepeatsByState,b.TotalStudyEvents,100.0*(CAST(RepeatsByState AS DECIMAL(18,4))/CAST(TotalStudyEvents AS DECIMAL(18,4))) WeightedAverage FROM RepeatImagingByState a
+SELECT a.State,a.RepeatsByState,b.TotalStudyEvents,100.0*(CAST(RepeatsByState AS DECIMAL(18,4))/CAST(TotalStudyEvents AS DECIMAL(18,4))) WeightedAverage 
+FROM RepeatImagingByState a
+INNER JOIN TotalStudyEventsByState b
+ON a.State=b.State
+ORDER BY WeightedAverage
+
+--Question 3 features associated with emergency room setting
+DROP TABLE IF EXISTS dbo.ErImaging
+SELECT a.*,CASE WHEN b.state IS NULL THEN 'UNK' ELSE b.State END State
+INTO dbo.ErImaging
+FROM dbo.StudyData_Cleansed a
+LEFT OUTER JOIN raw.zip_code_database b
+ON a.zip=b.zip
+WHERE a.iser=1
+--39,237 total ER events
+--100% of ER visits are also hospital visits, which makes sense
+
+DROP TABLE IF EXISTS dbo.ErImaging_WeightAge
+SELECT a.PatientID,a.state,c.AgeGroup,CAST(weight/((height/100.0)*(height/100.0)) AS INT) BMI,d.BMIGroup
+INTO dbo.ErImaging_WeightAge
+FROM dbo.ErImaging a
+INNER JOIN dbo.PatientData_Cleansed b
+on a.PatientID=b.PatientID
+INNER JOIN dbo.Age c
+ON b.age BETWEEN c.MinAge and c.MaxAge
+INNER JOIN dbo.BMI d
+ON CAST(CAST(weight AS FLOAT)/((height/100.0)*(height/100.0)) AS FLOAT) BETWEEN CAST(d.MinBMI AS DECIMAL(18,4)) and CAST(d.MaxBMI AS DECIMAL(18,4))
+
+SELECT PatientID,count(1) FROM dbo.ErImaging_WeightAge
+group by PatientID
+having count(1)>1
+--there are only 23 repeat ER visits in this set out of 39,000 which is good. We would not want multiple screenings in the ER as a large % of the population
+--with a result set so small, it's not something that I think we need to examine for systemic issues
+
+--baseline BMI check
+SELECT * FROM dbo.ErImaging_WeightAge
+WHERE BMI>25
+--only 114 rows, no obese patients in the entire set. Here too as with the others, BMI does not appear to have a significant correlation to ER visits, and 
+--there don't seem to be any obese patients in the data set.
+
+DROP TABLE IF EXISTS dbo.ErEventsByState
+SELECT State,COUNT(1) Events
+INTO dbo.ErEventsByState
+FROM dbo.ErImaging_WeightAge
+GROUP BY State
+
+select * FROM TotalStudyEventsByState
+
+--ErEventsByState.csv
+DROP TABLE IF EXISTS ErEventsByState_Output
+SELECT a.State,b.TotalStudyEvents,100.0*(CAST(Events AS DECIMAL(18,4))/CAST(TotalStudyEvents AS DECIMAL(18,4))) WeightedAverage
+INTO ErEventsByState_Output
+FROM dbo.ErEventsByState a
 INNER JOIN TotalStudyEventsByState b
 ON a.State=b.State
 ORDER BY WeightedAverage
@@ -158,7 +212,54 @@ ORDER BY WeightedAverage
 
 
 
+DROP TABLE IF EXISTS dbo.ErEventsByAgeState
+SELECT a.AgeGroup,a.State,COUNT(1) Patients
+INTO dbo.ErEventsByAgeState
+FROM dbo.ErImaging_WeightAge a
+INNER JOIN dbo.PatientData_Cleansed b
+on a.PatientID=b.PatientID
+INNER JOIN dbo.Age c
+ON b.age BETWEEN c.MinAge and c.MaxAge
+group by a.AgeGroup,a.State
+ORDER BY State,AgeGroup
 
---not sure why the values of 17 and 25 arent joining to my bmi range table, despite the fact that between should be inclusive and i properly cast everything as float
---only patients with BMI=17 or 25 are appearing here, on the cutoff. only 94 rows.
---despite this, most patients appear in the normal range. High BMI doesnt appear to correlate to repeat imaging
+DROP TABLE IF EXISTS ErImagingState
+select state,count(1) Events 
+INTO ErImagingState
+FROM ErImaging
+group by state
+
+
+--ErEventsByStateAndAge.csv
+DROP TABLE IF EXISTS ErEventsByAgeState_Output
+SELECT a.AgeGroup,a.State,a.Patients,b.Events TotalEvents,100.0*(CAST(Patients AS DECIMAL(18,4))/CAST(Events AS DECIMAL(18,4))) Weight
+--INTO ErEventsByAgeState_Output
+FROM ErEventsByAgeState a
+INNER JOIN erimagingstate b
+on a.state=b.state
+
+DROP TABLE IF EXISTS TotalEventsByAge
+SELECT AgeGroup,count(1) Events
+INTO TotalEventsByAge
+FROM StudyData_Cleansed a
+INNER JOIN PatientData_Cleansed b
+on a.PatientID=b.PatientID
+INNER JOIN Age c
+ON b.age BETWEEN c.MinAge and c.MaxAge
+GROUP BY AgeGroup
+
+DROP TABLE IF EXISTS ErEventsByAge
+SELECT AgeGroup,SUM(Patients) Patients
+INTO ErEventsByAge
+FROM ErEventsByAgeState a
+GROUP BY AgeGroup
+
+DROP TABLE IF EXISTS ErEventsByAge_Output
+SELECT a.AgeGroup,a.Patients ErEvents,b.Events TotalEvents,100.0*(CAST(Patients AS DECIMAL(18,4))/CAST(Events AS DECIMAL(18,4))) ErPercentage 
+INTO ErEventsByAge_Output
+FROM ErEventsByAge a
+INNER JOIN TotalEventsByAge b
+ON a.AgeGroup=b.AgeGroup
+
+
+
